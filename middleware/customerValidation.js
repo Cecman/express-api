8c133e367b9d4ccfe6e20e9975a618731fd4367c
Tj <jane.trajkoski@gmail.com>
2021-04-06 14:17:30 +0200

    added customer validation

--- middleware/customerValidation.js
+++ middleware/customerValidation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 module.exports((customer) => {
   const schema = Joi.object({
     isGold: Joi.boolean(),
-    name: Joi.string().required().min(3),
+    name: Joi.string().required().min(2),
     phone: Joi.number(),
   });
   return schema.validate(customer);
