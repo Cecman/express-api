e49ee666f8b30f13752e81df7e14f98d338524dc
Cecman <jane.trajkoski@gmail.com>
2021-04-07 13:47:28 +0200

    added route handlers for post/delete/update for the category document

--- routes/handlers/categoriesHandler.js
+++ routes/handlers/categoriesHandler.js
@@ -1,6 +1,9 @@
 const { Category } = require("../../db/models/category");
 
-const getCategoriesHandler = async () => {};
+const getCategoriesHandler = async (req, res) => {
+  const categories = await Category.find();
+  res.send(categories);
+};
 
 const createCategoryHandler = async (req, res) => {
   const category = new Category({
@@ -10,9 +13,23 @@ const createCategoryHandler = async (req, res) => {
   res.send(result);
 };
 
-const updateCategoryHandler = async () => {};
+const updateCategoryHandler = async (req, res) => {
+  const query = { name: req.params.name };
+  const updated = await Category.updateOne(query, { name: req.body.name });
 
-const deleteCategoryHandler = async () => {};
+  res.send(updated);
+};
+
+const deleteCategoryHandler = async (req, res) => {
+  const deleted = await Category.findOneAndDelete({ name: req.params.name });
+  if (!deleted) {
+    return res
+      .status(404)
+      .send(`There is no ${req.params.name} currently in our database.`);
+  }
+
+  res.send(deleted);
+};
 
 module.exports = {
   getCategoriesHandler,
