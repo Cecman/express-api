e8734d1a32ff15fc650dbdc72c60c09bc687371f
Cecman <jane.trajkoski@gmail.com>
2021-04-08 12:37:29 +0200

    added authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,7 @@
 const express = require("express");
-const userSchema = require("../db/models/register");
-const validator = require("../middleware/authentication");
-const bcrypt = require("bcrypt");
 const router = express.Router();
-const _ = require("lodash");
+const authHandler = require("./handlers/authHandler");
 
-router.post("/", async (req, res) => {
-  const { error } = validator(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const user = await userSchema.findOne({ email: req.body.email });
-
-  if (!user) {
-    return res.status(400).send(`Invalid email or password`);
-  }
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-
-  if (!validPassword) {
-    return res.status(400).send(`Invalid email or password`);
-  }
-
-  res.send(true);
-});
+router.post("/", authHandler);
 
 module.exports = router;
---
+++ routes/handlers/authHandler.js
@@ -0,0 +1,25 @@
+const userSchema = require("../../db/models/register");
+const validator = require("../../middleware/authentication");
+const bcrypt = require("bcrypt");
+
+const authHandler = async (req, res) => {
+  const { error } = validator(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const user = await userSchema.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(400).send(`Invalid email or password`);
+  }
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (!validPassword) {
+    return res.status(400).send(`Invalid email or password`);
+  }
+
+  res.send(true);
+};
+
+module.exports = authHandler;
